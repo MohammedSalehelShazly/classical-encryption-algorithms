697be03e2e2177efd0b4bf286bb57fb8096ee4e9
Mohammed Saleh <71941881+MohammedSalehelShazly@users.noreply.github.com>
2022-12-27 22:54:36 +0200

    finally

--- classical_encryption_algorithms.py
+++ classical_encryption_algorithms.py
@@ -23,17 +23,16 @@ brutForceAttack('', 3, 'abc')
 #####................Encryption and Decryption of Caesar-Cipher with equation................#####
 def EncryptCaesarCipher(plainText, key):
     cipherText = ''
-    for plainTextLetter in plainText: # this is plainText @#$%^^&**(*(
-            asci = ord(plainTextLetter) # convert to unicode code: a = 97
-            cipherText += chr(asci+key) # convert unicode code to character 100 = c
+    for plainTextLetter in plainText:
+        asci = ord(plainTextLetter) # convert to unicode code e.g: a = 97
+        cipherText += chr(asci+key) # convert unicode code to character e.g: 100 = c
     return cipherText
 
 def DecryptCaesarCipher(cipherText, key):
     plainText = ''
     for cipherTextLetter in cipherText:
-            asci = ord(cipherTextLetter)
-            plainText += chr(asci-key)
-
+        asci = ord(cipherTextLetter)
+        plainText += chr(asci-key)
     return plainText
 
 cipherText = EncryptCaesarCipher('this is plainText with some symbols @#$% ^_^ &**(*(' , 15)
@@ -115,30 +114,26 @@ def egcd(a, b):
     else:
         g, y, x = egcd(b % a, a)
         return (g, x - (b // a) * y, y)
-
-def modinv(a):
+def modInverse(a):
     m = 26
     g, x, y = egcd(a, m)
     if g != 1:
         raise Exception('modular inverse does not exist')
     else:
         return x % m
-
 def affineCipherEncryption(plainText, k1, k2):
     cipherText = ''
     for plainTextLetter in plainText:
         newIndex = (k1*alphabet.find(plainTextLetter) + k2) % len(alphabet)
         cipherText += alphabet[newIndex]
     return cipherText
-
-
 def affineCipherDecryption(cypherText, k1, k2):
     plainText = ''
     for cypherTextLetter in cypherText:
-        newIndex = (modinv(k1)) * (alphabet.find(cypherTextLetter) - k2) % len(alphabet)
+        newIndex = (modInverse(k1)) * (alphabet.find(cypherTextLetter) - k2) % len(alphabet)
         plainText += alphabet[newIndex]
     return plainText
-
+print('Affine Cipher....')
 cipherText = affineCipherEncryption('This is plainText', 7, 10) # (space) isn't in alphabet array so, it will encrypt to the latest alphabet (Z)
 print('cipherText is ', cipherText)
 plainText = affineCipherDecryption(cipherText, 7, 10)
@@ -165,8 +160,6 @@ def transpositionEncryption(plainText, depth):
     for cypherTextLines in lines:
         cypherText += cypherTextLines
     return cypherText
-
-
 def transpositionDecryption(cypherText, depth):
     plainText = ''
     lines = []
@@ -183,7 +176,6 @@ def transpositionDecryption(cypherText, depth):
             if len(lines[linesIndex]) > lettersIndex:
                 plainText += lines[linesIndex][lettersIndex]
             else: break
-        
     return plainText
 
 print('Transposition Cipher...')
@@ -192,8 +184,3 @@ print('CypherText is =>',transpositionEncryption1)
 transpositionDecryption1 = transpositionDecryption(transpositionEncryption1, 4)
 print('PlainText is =>',transpositionDecryption1)
 
-
-
-
-# .........................Columner Cipher
-# .........................S DES
